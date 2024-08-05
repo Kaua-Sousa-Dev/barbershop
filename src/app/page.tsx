e9c0d5183c51aef70b1302fc0268c6373324bd3f
Kaua <sousakaua7@gmail.com>
2024-08-05 15:50:03 -0300

    chore: add shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 "use client"
 
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
   return (
     <>
       <h1>Home Page</h1>
+      <Button>Click</Button>
     </>
   );
 }
