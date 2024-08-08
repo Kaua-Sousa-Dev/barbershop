e80f6fefb7fee76cc9a53535be725740fd471c2b
Kaua <sousakaua7@gmail.com>
2024-08-08 14:57:41 -0300

    feat: add barbershop component

--- src/app/_components/Search.tsx
+++ src/app/_components/Search.tsx
@@ -5,8 +5,12 @@ import Image from "next/image"
 import { Card, CardContent } from "./ui/card"
 import { Badge } from "./ui/badge"
 import { Avatar, AvatarImage } from "./ui/avatar"
+import { db } from "../_lib/prisma"
+import BarbershopItem from "./barbershopItem"
 
-const Search = () => {
+const Search = async () => {
+  // chamar banco de dados
+  const barbershops = await db.barbershop.findMany({})
   return (
     <>
       {/* Texto */}
@@ -29,11 +33,15 @@ const Search = () => {
             alt="Agende nos melhores com BarberShop"
             fill
             className="rounded-xl object-cover"
+            quality={100}
           />
         </div>
 
         {/* Agendamento */}
-        <Card className="mt-6">
+        <h2 className="mb-3 mt-6 text-xl font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <Card>
           <CardContent className="flex justify-between p-0">
             {/* Esquerda */}
             <div className="flex flex-col gap-2 py-5 pl-5">
@@ -51,11 +59,20 @@ const Search = () => {
             {/* Direita */}
             <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
               <p className="text-sm">Agosto</p>
-              <p className="text-2xl">05</p>
+              <p className="text-2xl">04</p>
               <p className="text-sm">20:00</p>
             </div>
           </CardContent>
         </Card>
+
+        <h2 className="mb-3 mt-5 text-xl font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
     </>
   )
