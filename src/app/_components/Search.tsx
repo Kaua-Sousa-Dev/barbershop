ba14edb5c2e6acd6769d0825352e1ce969b3c720
Kaua <sousakaua7@gmail.com>
2024-08-19 19:12:25 -0300

    feat: ensure bookings are listed on bookings page

--- src/app/_components/Search.tsx
+++ src/app/_components/Search.tsx
@@ -1,35 +1,56 @@
 import Image from "next/image"
-import { Card, CardContent } from "./ui/card"
-import { Badge } from "./ui/badge"
-import { Avatar, AvatarImage } from "./ui/avatar"
 import { db } from "../_lib/prisma"
 import BarbershopItem from "./barbershopItem"
 import SearchButton from "../_constant/SearchButton"
 import InputSearch from "./SearchInput"
+import BookingItem from "./booking-item"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../_lib/auth"
 
+// No seu componente Search:
 const Search = async () => {
-  // chamar banco de dados
+  // Buscar barbearias
+  const session = await getServerSession(authOptions)
   const barbershops = await db.barbershop.findMany({})
   const popularBarbershops = await db.barbershop.findMany({
     orderBy: {
       name: "desc",
     },
   })
+
+  // Buscar agendamentos
+  const confirmedBookings = session?.user
+    ? await db.booking.findMany({
+        where: {
+          userId: (session?.user as any).id,
+          date: {
+            gte: new Date(),
+          },
+        },
+        include: {
+          service: {
+            include: {
+              barbershop: true,
+            },
+          },
+        },
+        orderBy: {
+          date: "asc",
+        },
+      })
+    : []
+
   return (
     <>
-      {/* Texto */}
       <div className="p-5">
         <h2 className="text-xl font-bold">Olá, ser humano</h2>
         <p>Domingo, 04 de agosto</p>
 
-        {/* Busca */}
         <div className="mt-6">
           <InputSearch />
         </div>
-        {/* Busca Rápida*/}
         <SearchButton />
 
-        {/* Banner */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             src="/Banner01.png"
@@ -40,35 +61,14 @@ const Search = async () => {
           />
         </div>
 
-        {/* Agendamento */}
-        <h2 className="mb-3 mt-6 text-xl font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            {/* Esquerda */}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
+        {/* Renderizar agendamentos */}
+        <div className="mt-4 flex gap-3 overflow-x-auto">
+          {confirmedBookings.map((booking) => (
+            <BookingItem key={booking.id} booking={booking} />
+          ))}
+        </div>
 
-              <div className="flex items-center gap-2">
-                <Avatar className="h-8 w-8">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sm">Barbearia Ks.Lima</p>
-              </div>
-            </div>
-
-            {/* Direita */}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">04</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
-
-        <h2 className="mb-3 mt-5 text-xl font-bold uppercase text-gray-400">
+        <h2 className="mb-3 mt-5 text-base font-bold uppercase text-gray-400">
           Recomendados
         </h2>
         <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
@@ -77,8 +77,8 @@ const Search = async () => {
           ))}
         </div>
 
-        <h2 className="mb-3 mt-5 text-xl font-bold uppercase text-gray-400">
-          Recomendados
+        <h2 className="mb-3 mt-5 text-base font-bold uppercase text-gray-400">
+          Populares
         </h2>
         <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
           {popularBarbershops.map((barbershop) => (
