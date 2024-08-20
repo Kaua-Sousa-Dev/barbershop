108c2df34e5a27134f4b3412432cd64ebb1f3ddf
Kaua <sousakaua7@gmail.com>
2024-08-20 17:47:06 -0300

    refactor: ensure decimal warning is not shown on console

--- src/app/_components/Search.tsx
+++ src/app/_components/Search.tsx
@@ -73,7 +73,10 @@ const Search = async () => {
             </h2>
             <div className="mt-4 flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
               {confirmedBookings.map((booking) => (
-                <BookingItem key={booking.id} booking={booking} />
+                <BookingItem
+                  key={booking.id}
+                  booking={JSON.parse(JSON.stringify(booking))}
+                />
               ))}
             </div>
           </>
